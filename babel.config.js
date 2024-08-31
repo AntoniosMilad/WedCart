f13c91a7f50b98912cf20825d9f52dea7d977b9b
antonios.milad <antonios.milad@waseet.net>
2024-08-31 23:48:49 +0300

    add babel.config

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,39 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      '@babel/plugin-transform-class-properties',
+      {loose: true}, // Ensure 'loose' mode is set consistently
+    ],
+    [
+      '@babel/plugin-transform-private-methods',
+      {loose: true}, // Ensure 'loose' mode is set consistently
+    ],
+    [
+      '@babel/plugin-transform-private-property-in-object',
+      {loose: true}, // Ensure 'loose' mode is set consistently
+    ],
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          //  tests: ['./tests/'],
+          //  "@components": "./src/components",
+        },
+      },
+    ],
+    [
+      'module:react-native-dotenv',
+      {
+        moduleName: '@env',
+        path: '.env',
+        blacklist: null,
+        whitelist: null,
+        safe: false,
+        allowUndefined: true,
+      },
+    ],
+  ],
 };
